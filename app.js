f6c701f61c035671e841705bc1c0b1d788e2be30
ashishnetsec <aashish.9784@gmail.com>
2025-11-06 18:07:20 +0530

    section 2 completed

--- app.js
+++ app.js
@@ -15,9 +15,60 @@ search.addEventListener(
 
 signinbtn.addEventListener(
     'click',
-    function(){
+    function () {
         form.classList.toggle("input-active")
         inputBox.classList.remove('input-active')
     }
 )
+
+var navbar_bar = document.querySelector('#navbar-bar')
+var actionbtn = document.querySelector('.action-btn')
+
+navbar_bar.addEventListener(
+    'click',
+    function () {
+        // console.log('hi')
+        actionbtn.classList.toggle('show-action-btn')
+    }
+)
+
+
+async function responseApi() {
+    var response = await fetch('https://dummyjson.com/products')
+    var data = await response.json();
+
+    makeCards(data.products)
+
+
+}
+
+var npbottom = document.querySelector('.npc-bottom')
+
+
+function makeCards(apiProducts) {
+
+    for(data of apiProducts){
+    newDiv = document.createElement('div')
+    newDiv.classList.add('npcb-card')
+
+    newDiv.innerHTML = `
+                        <img src="${data.thumbnail}" alt="" width="100%" height="100%">
+                        <div>
+                        <span>${data.title}</span>
+                        <h4>$ ${data.price}</h4>
+                        </div>  
+    `
+    npbottom.appendChild(newDiv)
+    }
+
+    console.log(data)
+
+
+}
+
+
+responseApi()
+
+
+
 
